e5da7f88a2eb8b7a440cdceeffaa6c0561573ccb
Ubuntu <ubuntu@ip-172-31-85-0.ec2.internal>
2019-09-11 17:35:35 +0000

    added cart

--- coffeecultureclub/settings.py
+++ coffeecultureclub/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'accounts',
     'posts',
     'products',
+    'cart',
+
 
 
 
@@ -72,6 +74,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'cart.contexts.cart_contents'
             ],
         },
     },
--- coffeecultureclub/urls.py
+++ coffeecultureclub/urls.py
@@ -22,6 +22,8 @@ from django.views.generic import RedirectView
 from .settings import MEDIA_ROOT
 from products import urls as urls_products
 from products.views import all_products
+from cart import urls as urls_cart
+
 
 
 
@@ -34,6 +36,6 @@ urlpatterns = [
     url(r'^posts/', include('posts.urls')),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
     url(r'^products/', include(urls_products), name="products"),
-
+    url(r'^cart/', include(urls_cart)),
 
 ]
