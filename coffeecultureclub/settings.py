c3425ea6f29a1d785bb0e0ca07f737f8b7029ee7
Ubuntu <ubuntu@ip-172-31-85-0.ec2.internal>
2019-09-16 11:02:30 +0000

    fixing static

--- coffeecultureclub/settings.py
+++ coffeecultureclub/settings.py
@@ -147,8 +147,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
