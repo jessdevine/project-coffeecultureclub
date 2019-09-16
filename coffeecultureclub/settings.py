e59a8cb23f3e77c41156edbb670a779c8606541c
Ubuntu <ubuntu@ip-172-31-85-0.ec2.internal>
2019-09-16 10:02:09 +0000

    added heroku as allowed host

--- coffeecultureclub/settings.py
+++ coffeecultureclub/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import env
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,12 +22,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't909gw)kmi6@(4&mtrji)0)$hd7a!_vttrvw^wfs48jqu9138)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['4cc29d84d29d4342bee304e0a5ac70e3.vfs.cloud9.us-east-1.amazonaws.com']
+ALLOWED_HOSTS = ['4cc29d84d29d4342bee304e0a5ac70e3.vfs.cloud9.us-east-1.amazonaws.com',
+                'https://coffee-culture-club.herokuapp.com']
 
 
 # Application definition
@@ -84,12 +86,24 @@ WSGI_APPLICATION = 'coffeecultureclub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
