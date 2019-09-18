0b5162096edfde9a9e922a606a429bfdd3744708
Ubuntu <ubuntu@ip-172-31-85-0.ec2.internal>
2019-09-18 18:28:46 +0000

    added no results message

--- search/views.py
+++ search/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
 from products.models import Product
+from django.contrib import messages
+from django.db.models import Count
+
+# Results.objects.filter(event=some_event).annotate(Count('category'), distinct=True)
 
 # Create your views here.
 def do_search(request):
     products = Product.objects.filter(name__icontains=request.GET['q'])
-    return render(request, "products.html", {"products": products})
+    if products:
+        return render(request, "products.html", {"products": products})
+    else:
+        messages.error(request, "No Results")
+        return redirect(reverse('products'))
+
+
