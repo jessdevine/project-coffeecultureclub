98072f4380de7d6001a420f4c065268038b4ec5f
Ubuntu <ubuntu@ip-172-31-85-0.ec2.internal>
2019-09-19 12:32:59 +0000

    New message if no search results

--- search/views.py
+++ search/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, reverse, redirect
 from products.models import Product
 from django.contrib import messages
-from django.db.models import Count
 
-# Results.objects.filter(event=some_event).annotate(Count('category'), distinct=True)
 
 # Create your views here.
 def do_search(request):
